3328509d91bb876e102f9644b3b65feda171c29b
Ruslan <ruslan.khasanov.03@mail.ru>
2024-12-05 23:54:35 +0300

    new modul utils.py

---
+++ utils.py
@@ -0,0 +1,23 @@
+import pygame
+
+WIDTH = 800
+HEIGHT = 600
+
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GREY = (101, 101, 101)
+RED = (235, 15, 15)
+ORANGE = (237, 118, 14)
+BLUE = (15, 15, 235)
+YELLOW = (255, 255, 0)
+
+FPS = 60
+
+clock = pygame.time.Clock()
+
+# Глобальные переменные
+lasers = []
+asteroids = []
+points = [0, 0]
+MAX_ASTEROIDS = 10
+game_time = 30
